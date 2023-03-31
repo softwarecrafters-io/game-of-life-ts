3075e67d2800768fbfed3775fd58c304525d6dfb
Miguel A. Gómez <>
2023-03-31 16:32:10 +0100

    green: Any dead cell with exactly three live neighbors becomes a live cell on the next generation

--- src/tests/gameOfLife.test.ts
+++ src/tests/gameOfLife.test.ts
@@ -18,9 +18,14 @@ class Cell {
 		if (this.status === CellStatus.Alive) {
 			if(numberOfNeighbors == 2 || numberOfNeighbors == 3)
 				return CellStatus.Alive;
-
+			return CellStatus.Dead;
 		}
-		return CellStatus.Dead;
+		else{
+			if(numberOfNeighbors == 3) {
+				return CellStatus.Alive;
+			}
+			return CellStatus.Dead;
+		}
 	}
 }
 
@@ -33,11 +38,15 @@ describe('In the Game of Life', ()=>{
 	it('Any live cell with two or three live neighbors lives on to the next generation', ()=>{
 		expect(new Cell(CellStatus.Alive).regenerate(2)).toBe(CellStatus.Alive);
 		expect(new Cell(CellStatus.Alive).regenerate(3)).toBe(CellStatus.Alive);
-		expect(new Cell(CellStatus.Dead).regenerate(3)).toBe(CellStatus.Dead);
+		expect(new Cell(CellStatus.Dead).regenerate(4)).toBe(CellStatus.Dead);
 	});
 
 	it('Any live cell with more than three live neighbors dies, as if by overcrowding', ()=>{
 		expect(new Cell(CellStatus.Alive).regenerate(4)).toBe(CellStatus.Dead);
 		expect(new Cell(CellStatus.Dead).regenerate(4)).toBe(CellStatus.Dead);
+	});
+
+	it('Any dead cell with exactly three live neighbors becomes a live cell on the next generation', ()=> {
+		expect(new Cell(CellStatus.Dead).regenerate(3)).toBe(CellStatus.Alive);
 	});
 });
