faea95e19e9cc5c51b7c59cde5608cf2fe7064a3
Miguel A. Gómez <>
2023-03-31 18:18:32 +0100

    green: generates the next state of the world

--- src/tests/world.test.ts
+++ src/tests/world.test.ts
@@ -1,4 +1,5 @@
 import { Cell, CellStatus } from "../core/cell";
+
 const { Dead, Alive } = CellStatus;
 
 class World{
@@ -10,17 +11,36 @@ class World{
   }
 
   aliveNeighbors(row: number, column: number) {
+    return (
+      this.alivePreviousRowNeighbors(row, column) +
+      this.aliveColumnNeighbors(row, column) +
+      this.aliveNextRowNeighbors(row, column)
+    );
+  }
+
+  private alivePreviousRowNeighbors(row: number, column: number) {
     let aliveNeighbors = 0;
-    if(row + 1 < this.cellMatrix.length) {
-      if(this.isAliveCellAt(row + 1, column)) {
+    if (row - 1 >= 0) {
+      if (this.isAliveCellAt(row - 1, column)) {
+        aliveNeighbors++;
+      }
+      aliveNeighbors += this.aliveColumnNeighbors(row - 1, column);
+    }
+    return aliveNeighbors;
+  }
+
+  private aliveNextRowNeighbors(row: number, column: number) {
+    let aliveNeighbors = 0;
+    if (row + 1 < this.cellMatrix.length) {
+      if (this.isAliveCellAt(row + 1, column)) {
         aliveNeighbors++;
       }
       aliveNeighbors += this.aliveColumnNeighbors(row + 1, column);
     }
-    return aliveNeighbors + this.aliveColumnNeighbors(row, column,);
+    return aliveNeighbors;
   }
 
-  private aliveColumnNeighbors(row: number, column: number, ) {
+  private aliveColumnNeighbors(row: number, column: number) {
     let aliveNeighbors = 0;
     const previousColumn = column - 1;
     if (previousColumn >= 0 && this.isAliveCellAt(row, previousColumn)) {
@@ -37,31 +57,62 @@ class World{
   private isAliveCellAt(row: number, column: number) {
     return this.cellMatrix[row][column].isAlive();
   }
+
+  nextGeneration() {
+    const nextGenerationCellMatrix = this.cellMatrix.map((row, rowIndex) =>
+      row.map((cell, columnIndex) =>
+        cell.regenerate(this.aliveNeighbors(rowIndex, columnIndex))));
+    return new World(nextGenerationCellMatrix);
+  }
 }
 
 describe('The World', ()=>{
-    it('creates a cell matrix for a given cell status', ()=>{
-      const initialStatus = [
-        [Dead, Dead],
-        [Dead, Alive],
-      ];
+  it('creates a cell matrix for a given cell status', ()=>{
+    const initialStatus = [
+      [Dead, Dead],
+      [Dead, Alive],
+    ];
 
-      const world = World.createFrom(initialStatus);
+    const world = World.createFrom(initialStatus);
 
-      expect(world.cellMatrix).toEqual([
-          [Cell.create(Dead), Cell.create(Dead)],
-          [Cell.create(Dead), Cell.create(Alive)],
-        ]);
-    });
+    expect(world.cellMatrix).toEqual([
+        [Cell.create(Dead), Cell.create(Dead)],
+        [Cell.create(Dead), Cell.create(Alive)],
+      ]);
+  });
 
-    it('gets alive neighbors for a given coordinates', ()=>{
-      expect(World.createFrom([[Dead]]).aliveNeighbors(0,0)).toBe(0);
-      expect(World.createFrom([[Dead, Alive]]).aliveNeighbors(0,0)).toBe(1);
-      expect(World.createFrom([[Alive, Dead]]).aliveNeighbors(0,1)).toBe(1);
-      expect(World.createFrom([[Alive, Dead, Alive]]).aliveNeighbors(0,1)).toBe(2);
-      expect(World.createFrom([
-        [Alive, Dead, Alive],
-        [Alive, Alive, Alive],
-      ]).aliveNeighbors(0,1)).toBe(5);
-    });
+  it('gets alive neighbors for a given coordinates', ()=>{
+    expect(World.createFrom([[Dead]]).aliveNeighbors(0,0)).toBe(0);
+    expect(World.createFrom([[Dead, Alive]]).aliveNeighbors(0,0)).toBe(1);
+    expect(World.createFrom([[Alive, Dead]]).aliveNeighbors(0,1)).toBe(1);
+    expect(World.createFrom([[Alive, Dead, Alive]]).aliveNeighbors(0,1)).toBe(2);
+    expect(World.createFrom([
+      [Alive, Dead, Alive],
+      [Alive, Alive, Alive],
+    ]).aliveNeighbors(0,1)).toBe(5);
+    expect(World.createFrom([
+      [Alive, Dead, Alive],
+      [Dead, Dead, Dead],
+    ]).aliveNeighbors(0,1)).toBe(2);
+    expect(World.createFrom([
+      [Alive, Alive, Alive],
+      [Alive, Dead, Alive],
+      [Alive, Alive, Alive]]).aliveNeighbors(1, 1)).toBe(8);
+  });
+
+  it('generates the next state of the world', ()=>{
+    const initialStatus = [
+      [Dead, Alive, Dead],
+      [Dead, Alive, Dead],
+      [Dead, Alive, Dead],
+    ];
+
+    const nextGeneration = World.createFrom(initialStatus).nextGeneration();
+
+    expect(nextGeneration.cellMatrix).toEqual([
+      [Cell.create(Dead), Cell.create(Dead), Cell.create(Dead)],
+      [Cell.create(Alive), Cell.create(Alive), Cell.create(Alive)],
+      [Cell.create(Dead), Cell.create(Dead), Cell.create(Dead)],
+    ]);
+  })
 });
